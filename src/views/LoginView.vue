edaec59e8968f3c41bdda782ec089f860f21ca56
chuan <101337985+Chuan-OWO@users.noreply.github.com>
2023-06-02 22:05:14 +0800

    註冊 pages 註冊功能實作

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -7,23 +7,29 @@
 
         <div class="col-lg-7 text-center text-lg-start">
             <h1 class="display-4 fw-bold lh-1 text-body-emphasis mb-3">
-                Vertically centered hero sign-up form
+                登入
             </h1>
             <p class="col-lg-10 fs-4">
-                Below is an example form built entirely with Bootstrap’s form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.
+                登入登入登入登入登入
             </p>
         </div>
 
 
         <div class="col-md-10 mx-auto col-lg-5">
 
-            <form class="p-4 p-md-5 border rounded-3 bg-body-tertiary">
+            <form 
+                class="p-4 p-md-5 border rounded-3 bg-body-tertiary"
+
+                @submit.prevent="login"
+            >
                 <div class="form-floating mb-3">
                     <input 
                         type="email" 
                         class="form-control" 
                         id="floatingInput" 
                         placeholder="name@example.com"
+
+                        v-model="user.email"
                     >
                     <label for="floatingInput">Email address</label>
                 </div>
@@ -33,15 +39,17 @@
                         class="form-control" 
                         id="floatingPassword" 
                         placeholder="Password"
+
+                        v-model="user.password"
                     >
                     <label for="floatingPassword">密碼</label>
                 </div>
-                <div class="checkbox mb-3">
+                <!-- <div class="checkbox mb-3">
                     <label>
                         <input type="checkbox" value="remember-me"> 
                         Remember me
                     </label>
-                </div>
+                </div> -->
                 <button class="w-100 btn btn-lg btn-primary" type="submit">
                     登入
                 </button>
@@ -57,7 +65,40 @@
   </div>
 </template>
 
-<script setup>
+<script>
+import axios from 'axios';
 
+export default{
+    data() {
+        return {
+            user:{
+                email:'',
+                password:'',
+            }
+        }
+    },
+    methods:{
+        async login(){
+            // console.log('login');
+            const api = `${import.meta.env.VITE_APP_API}${import.meta.env.VITE_LOGIN}`
+            // console.log(api);
+            
+            try {
+                const res = await this.axios.post(api,this.user)
+                console.log(res);
+                
+                const token = res.data.token;
+
+                localStorage.setItem('token', token);
+                alert('登入成功')
+                this.$router.push('/dashboard');
+
+            } catch (error) {
+                console.error(error);
+                alert(error)
+            }
+        }
+    }
+}
 </script>
 
