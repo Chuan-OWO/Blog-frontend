ea2ea429e95599f8d1935f5651a38962d8a6a55a
chuan <101337985+Chuan-OWO@users.noreply.github.com>
2023-06-02 22:06:29 +0800

    註冊 功能實作

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -7,33 +7,43 @@
   
           <div class="col-lg-7 text-center text-lg-start">
               <h1 class="display-4 fw-bold lh-1 text-body-emphasis mb-3">
-                  Vertically centered hero sign-up form
+                  註冊
               </h1>
               <p class="col-lg-10 fs-4">
-                  Below is an example form built entirely with Bootstrap’s form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.
+                註冊註冊註冊註冊註冊
               </p>
           </div>
   
   
           <div class="col-md-10 mx-auto col-lg-5">
   
-              <form class="p-4 p-md-5 border rounded-3 bg-body-tertiary">
+              <form 
+                class="p-4 p-md-5 border rounded-3 bg-body-tertiary"  
+
+                @submit.prevent="sign"
+              >
                   <div class="form-floating mb-3">
                       <input 
                           type="email" 
                           class="form-control" 
                           id="floatingInput" 
                           placeholder="name@example.com"
+
+                        v-model="user.email"
                       >
                       <label for="floatingInput">Email address</label>
+
+                      
                   </div>
 
                   <div class="form-floating mb-3">
                       <input 
-                          type="email" 
+                          type="text"
                           class="form-control" 
                           id="floatingInput" 
                           placeholder="username"
+                        
+                        v-model="user.username"
                       >
                       <label for="floatingInput">username</label>
                   </div>
@@ -44,6 +54,8 @@
                           class="form-control" 
                           id="floatingPassword" 
                           placeholder="Password"
+
+                          v-model="user.password"
                       >
                       <label for="floatingPassword">密碼</label>
                   </div>
@@ -63,8 +75,38 @@
     </div>
   </template>
   
-  <script setup>
-  
+  <script >
+import VueAxios from 'vue-axios';
+
+  export default{
+    data() {
+        return {
+            user:{
+                email:'',
+                username:'',
+                password:'',
+            }
+        }
+    },
+    methods:{
+        async sign(){
+            // console.log('sign');
+            
+            const api = `${import.meta.env.VITE_APP_API}${import.meta.env.VITE_SIGN}`
+
+            try {
+                const res = await this.axios.post(api,this.user)
+
+                console.log(res);
+
+                this.$router.push('/login');
+            } catch (error) {
+                 console.error(error);
+                 alert(error)
+            }
+        }
+    }
+  }
   </script>
   
   
