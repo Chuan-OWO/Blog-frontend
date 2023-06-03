a1e6317e25430f67e4662537a064ec8bf788b1b8
chuan <101337985+Chuan-OWO@users.noreply.github.com>
2023-06-03 09:23:25 +0800

    改縮排

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,14 @@
   <header class="p-3 text-bg-dark">
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-       
-
+        
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
           <li><RouterLink  to="/" class="text-white nav-link px-2  ">首頁</RouterLink></li>
           <li><RouterLink  to="/about" class="text-white nav-link px-2 ">About</RouterLink></li>
-         
+
           <!-- <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
           <li><a href="#" class="nav-link px-2 text-white">Features</a></li> -->
-       
+
         </ul>
 
         <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
@@ -24,8 +23,8 @@
             Login
           </button>
           <button
-           @click="$router.push('/sign')"          
-           type="button" class="btn btn-warning">Sign-up</button>
+          @click="$router.push('/sign')"          
+          type="button" class="btn btn-warning">Sign-up</button>
         </div>
       </div>
     </div>
@@ -35,11 +34,11 @@
   <RouterView />
 </template>
 
-<script >
-import { RouterLink, RouterView } from 'vue-router'
-import { inject } from 'vue'
-
+<script>
+  import { RouterLink, RouterView } from 'vue-router'
+  
 </script>
+
 
 <style lang="scss" >/*
   .container{
