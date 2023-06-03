ac1e909d5b50b85167d25821791f40955cd54894
chuan <101337985+Chuan-OWO@users.noreply.github.com>
2023-06-03 09:22:27 +0800

    改排版

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,21 +1,20 @@
 <template>
     <div class="container col-xl-10 col-xxl-8 px-4 py-5">
-  
-  
-      <div class="row align-items-center g-lg-5 py-5">
-  
-  
-          <div class="col-lg-7 text-center text-lg-start">
-              <h1 class="display-4 fw-bold lh-1 text-body-emphasis mb-3">
-                  註冊
-              </h1>
-              <p class="col-lg-10 fs-4">
+
+
+    <div class="row align-items-center g-lg-5 py-5">
+
+
+        <div class="col-lg-7 text-center text-lg-start">
+            <h1 class="display-4 fw-bold lh-1 text-body-emphasis mb-3">
+                註冊
+            </h1>
+            <p class="col-lg-10 fs-4">
                 註冊註冊註冊註冊註冊
-              </p>
-          </div>
-  
-  
-          <div class="col-md-10 mx-auto col-lg-5">
+            </p>
+        </div>
+
+        <div class="col-md-10 mx-auto col-lg-5">
   
               <form 
                 class="p-4 p-md-5 border rounded-3 bg-body-tertiary"  
@@ -69,24 +68,22 @@
                       </small>
               </form>
   
-          </div>
-  
-      </div>
-    </div>
-  </template>
-  
-  <script >
-import VueAxios from 'vue-axios';
+        </div>
 
-  export default{
+    </div>
+    </div>
+</template>
+
+<script >
+export default{
     data() {
         return {
-            user:{
-                email:'',
-                username:'',
-                password:'',
-            }
+        user:{
+        email:'',
+        username:'',
+        password:'',
         }
+    }
     },
     methods:{
         async sign(){
@@ -101,12 +98,11 @@ import VueAxios from 'vue-axios';
 
                 this.$router.push('/login');
             } catch (error) {
-                 console.error(error);
-                 alert(error)
+                console.error(error);
+                alert(error)
             }
         }
     }
-  }
-  </script>
-  
-  
+}
+</script>
+
