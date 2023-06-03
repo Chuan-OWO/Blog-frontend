006cc15f6e30b5aedb35124e14ba50c0f4403deb
chuan <101337985+Chuan-OWO@users.noreply.github.com>
2023-06-03 09:22:06 +0800

    已經登入的用戶頁面

---
+++ src/views/UserView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container col-xl-10 col-xxl-8 px-4 py-5">
+    <h1>已經登入的用戶頁面</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const token = localStorage.getItem('token')
+    if (!token) {
+    // 沒登入 跳轉登入頁面
+    alert('未登入')
+    this.$router.push('/login')
+    }
+  }
+}
+</script>
